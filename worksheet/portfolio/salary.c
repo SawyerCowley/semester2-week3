d816bca8475953c7e468c05d0953978e76082f1b
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-02-20 10:19:39 +0000

    completed worksheet

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,8 +3,8 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Sawyer LH Cowley
+ * ID:201958891
  */
 
  int main( void ) {
@@ -18,6 +18,28 @@
     //printf("NI contribution £...",var_name);
     //printf("Tax contribution £...",var_name);
     //printf("Take home salary £...",var_name);
+
+    //setting variables 
+    int salary = 36250;
+    float national_insurance = 0.08;
+    float tax_rate = 0.15;
+    float take_home = 0;
+    
+    //calculating NI contribution
+    float ni_contribution = salary * national_insurance;
+    take_home = salary - ni_contribution;
+    //deducting minimum take home
+    float pre_tax = take_home - 12500;
+    //cacluating tax contribution 
+    float tax_contribution = pre_tax * tax_rate;
+    //deducting contribution from take home
+    take_home = take_home - tax_contribution;
+
+   //printing data
+    printf("Salary £%d\n",salary);
+    printf("NI contribution £%.2f\n",ni_contribution);
+    printf("Tax contribution £%.2f\n",tax_contribution);
+    printf("Take home salary £%.2f\n",take_home);
 
     return 0;
  }
