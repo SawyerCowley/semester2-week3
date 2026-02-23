5ff2b6b415e7d3b1fc3f86111111db56e7da8ed0
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-02-23 12:46:05 +0000

    minor changes

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -36,7 +36,7 @@
     take_home = take_home - tax_contribution;
 
    //printing data
-    printf("Salary £%d\n",salary);
+    printf("Salary £%i\n",salary);
     printf("NI contribution £%.2f\n",ni_contribution);
     printf("Tax contribution £%.2f\n",tax_contribution);
     printf("Take home salary £%.2f\n",take_home);
